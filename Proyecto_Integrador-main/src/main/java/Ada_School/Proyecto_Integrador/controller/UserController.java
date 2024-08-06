c901427fd637b46468bcb5b61177877bfab092ef
Naomidl <naomideleon@emilianisomascos.edu.gt>
2024-08-05 23:25:06 -0600

    Cambios realizados incluyendo Mondongo DB

--- Proyecto_Integrador-main/src/main/java/Ada_School/Proyecto_Integrador/controller/UserController.java
+++ Proyecto_Integrador-main/src/main/java/Ada_School/Proyecto_Integrador/controller/UserController.java
@@ -1,47 +1,69 @@
 package Ada_School.Proyecto_Integrador.controller;
 
 import Ada_School.Proyecto_Integrador.dto.UserDto;
+import Ada_School.Proyecto_Integrador.model.User;
 import Ada_School.Proyecto_Integrador.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users/")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/")
-    public ResponseEntity<List<UserDto>> findAll(){
-        List<UserDto> lista = this.userService.getAll();
-        return ResponseEntity.ok(lista);
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<UserDto> findById(@PathVariable("id") String id){
-        UserDto usuario = this.userService.getById(id);
-        return ResponseEntity.ok(usuario);
-    }
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+        public List<User> getAllUsers() {
+            return userService.getAllUsers();
+        }
 
-    @PostMapping("/")
-    public ResponseEntity<UserDto> save(@RequestBody UserDto userDto){
-        UserDto saved = this.userService.save(userDto);
-        return ResponseEntity.ok(saved);
-    }
+        @GetMapping("/{id}")
+        public ResponseEntity<User> getUserById (@PathVariable Long id){
+            Optional<User> user = userService.getUserById(id);
+            return user.map(u -> new ResponseEntity<>(u, HttpStatus.OK))
+                    .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+            public User getUserById (@PathVariable("id") String id){
+                return userService.getUserById(id);
+            }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<UserDto> update(@RequestBody UserDto userDto, @PathVariable("id") String id){
-        UserDto updated = this.userService.update(userDto, id);
-        return ResponseEntity.ok(updated);
-    }
+            @PostMapping
+            public User createUser (@RequestBody User user){
+                return userService.saveUser(user);
+            }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable("id") String id){
-        this.userService.delete(id);
-        return ResponseEntity.noContent().build();
+            @PutMapping("/{id}")
+            public ResponseEntity<User> updateUser (@PathVariable Long id, @RequestBody User user){
+                User updatedUser = userService.updateUser(id, user);
+                return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+                public User updateUser (@PathVariable("id") String id, @RequestBody User user){
+                    user.setId(id);  // Asegurarnos de que setId existe en la clase User
+                    return userService.saveUser(user);
+                }
+
+                @DeleteMapping("/{id}")
+                public ResponseEntity<Void> deleteUser (@PathVariable Long id){
+                    public void deleteUser (@PathVariable("id") String id){
+                        userService.deleteUser(id);
+                        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+                    }
+                }
+            }
+        }
     }
 }
--- Proyecto_Integrador-main/src/main/java/Ada_School/Proyecto_Integrador/service/UserService.java
+++ Proyecto_Integrador-main/src/main/java/Ada_School/Proyecto_Integrador/service/UserService.java
@@ -1,57 +1,44 @@
 package Ada_School.Proyecto_Integrador.service;
 
-import Ada_School.Proyecto_Integrador.dto.UserDto;
-import Ada_School.Proyecto_Integrador.entity.UserEntity;
+import Ada_School.Proyecto_Integrador.model.User;
 import Ada_School.Proyecto_Integrador.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
-@Service
-public class UserService {
+public interface UserService {
+    User createUser(User user);
 
-    @Autowired
-    private UserRepository userRepository;
+    List<User> getAllUsers();
 
-    public List<UserDto> getAll(){
-        return this.userRepository.findAll().stream()
-                .map(this::toDto)
-                .toList();
-    }
+    Optional<User> getUserById(Long id);
 
-    public UserDto getById(String id){
-        return this.userRepository.findById(id)
-                .map(this::toDto)
-                .orElse(null);
-    }
+    User updateUser(Long id, User user);
 
-    public UserDto save(UserDto user){
-        UserEntity entity = new UserEntity();
-        entity.setName(user.getName());
-        entity.setEmail(user.getEmail());
-        UserEntity entitySaved = this.userRepository.save(entity);
-        UserDto saved = this.toDto(entitySaved);
-        return saved;
-    }
+    void deleteUser(Long id);
 
-    public UserDto update(UserDto user, String id){
-        UserEntity entity = this.userRepository.findById(id)
-                .orElse(null);
-        entity.setName(user.getName());
-        entity.setEmail(user.getEmail());
-        UserEntity entitySaved = this.userRepository.save(entity);
-        UserDto update = this.toDto(entitySaved);
-        return update;
-    }
+    @Service
+    public class UserController {
 
-    public void delete(String id){
-        UserEntity entity = this.userRepository.findById(id)
-                .orElse(null);
-        this.userRepository.delete(entity);
-    }
+        @Autowired
+        private UserRepository userRepository;
 
-    private UserDto toDto(UserEntity entity){
-        return new UserDto(entity.getId(), entity.getName(), entity.getEmail());
+        public List<User> getAllUsers() {
+            return userRepository.findAll();
+        }
+
+        public User getUserById(String id) {
+            return userRepository.findById(id).orElse(null);
+        }
+
+        public User saveUser(User user) {
+            return  userRepository.save(user);
+        }
+
+        public void deleteUser(String id) {
+            userRepository.deleteById(id);
+        }
     }
 }
