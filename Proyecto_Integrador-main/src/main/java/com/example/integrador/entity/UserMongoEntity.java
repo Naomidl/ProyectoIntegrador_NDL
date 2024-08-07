a186204da5594691e3b18e2e9ef64a58c241c5d5
Naomidl <naomideleon@emilianisomascos.edu.gt>
2024-08-07 00:28:47 -0600

    Actualización con JUnit y Mockito

---
+++ Proyecto_Integrador-main/src/main/java/com/example/integrador/entity/UserMongoEntity.java
@@ -0,0 +1,104 @@
+package com.example.integrador.entity;
+
+import com.example.integrador.util.Role;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Document(collection = "users")
+public class UserMongoEntity implements UserDetails {
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private Role role;
+
+    // Constructor por defecto
+    public UserMongoEntity() {
+    }
+
+    // Constructor con parámetros
+    public UserMongoEntity(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    // Getters y setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
